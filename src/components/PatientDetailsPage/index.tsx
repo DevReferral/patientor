1a4f1ba3c43fcb91e0cc84611b3e44b0eab45a3b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-12 11:06:39 +0000

    12 May 2023, 16:36

--- src/components/PatientDetailsPage/index.tsx
+++ src/components/PatientDetailsPage/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Patient } from "../../types"
 
-const PatientDetails = () => {
+const PatientDetails = (patient:Patient) => {
+  
   return (
-    <div>PatientDetails</div>
+    <>
+      <h2>{patient.name}</h2>
+
+      {patient.ssn ? <h3>`ssh:${patient.ssn}`</h3>:'' }
+        <h3>Occupation : {patient.occupation}</h3>  
+
+    </>
   )
+  
 }
 
 export default PatientDetails
