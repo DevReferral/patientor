193dd1fd730538e13460e67f241485ad01ce3112
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-12 12:15:49 +0000

    12 May 2023, 17:45

--- src/components/PatientPage/index.tsx
+++ src/components/PatientPage/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import patients from '../../services/patients'
 import { Patient } from '../../types'
+import { getErrorMessage } from '../../utils'
 import PatientDetails from './PatientDetails'
 
 const PatientPage = () => {
@@ -13,6 +14,16 @@ const PatientPage = () => {
 
   const [error,setError] = useState('')
   
+  
+  const setErrorMessageWithTimeout = (errorMessage: string, timeout_in_ms: number) => {
+ 
+    setError(errorMessage);
+
+  setTimeout(() => {
+    setError('');
+  }, timeout_in_ms);
+};
+
   useEffect(() => {
     
     //if id is present , set patient in component load
@@ -21,19 +32,15 @@ const PatientPage = () => {
 
         patients.getOne(id)
         .then(data => setPatient(data))
-        .catch((err) =>{
-            
-          if(axios.isAxiosError(err)) {
-
-
-          }
-
-
-        
+        .catch((err) => {
+          
+          const errorMsg = getErrorMessage(err);
+          setErrorMessageWithTimeout(errorMsg,2000)
+          
         })
     }
 
-  },[])
+  },[id])
    
   return (
     <>
