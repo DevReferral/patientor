01311a6565a6770949469fd5917697c95237ea20
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-12 12:04:13 +0000

    12 May 2023, 17:34

--- src/components/PatientPage/index.tsx
+++ src/components/PatientPage/index.tsx
@@ -1,11 +1,39 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import patients from '../../services/patients'
 import { Patient } from '../../types'
 import PatientDetails from './PatientDetails'
 
 const PatientPage = () => {
    
+  const {id} = useParams()
+  
   const[patient,setPatient] = useState<Patient|null>(null)
 
+  const [error,setError] = useState('')
+  
+  useEffect(() => {
+    
+    //if id is present , set patient in component load
+
+    if(id) {
+
+        patients.getOne(id)
+        .then(data => setPatient(data))
+        .catch((err) =>{
+            
+          if(axios.isAxiosError(err)) {
+
+
+          }
+
+
+        
+        })
+    }
+
+  },[])
    
   return (
     <>
