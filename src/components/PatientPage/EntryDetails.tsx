df89f6b5980fc69dfc06ca4dd5a3b57bcd3ecd28
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-13 10:09:01 +0000

    13 May 2023, 15:39

--- src/components/PatientPage/EntryDetails.tsx
+++ src/components/PatientPage/EntryDetails.tsx
@@ -5,61 +5,65 @@ import CommonDiagnosisEntry from './CommonDiagnosisEntry';
 
 const EntryDetails = ({entry}:{entry:DiagnosisEntry}) => {
    
-   let details =null
- try{
-   switch (entry.type) {
+  let details =null
+  
+  try{
+     
+    switch (entry.type) {
 
-    case "HealthCheck":
-      details =<>
-       <div> Health Check Rating :{entry.healthCheckRating}</div>
-      </>
-    break;
-    case "Hospital":
-      details = <>
-       {entry.discharge && (<>
-          <div>Date of Discharge :{entry.discharge.date}</div>
-          <div>Criteria for Discharge :{entry.discharge.criteria}</div>
-       </>)}
-      </>
+      case "HealthCheck":
+        details =<>
+        <div> Health Check Rating :{entry.healthCheckRating}</div>
+        </>
       break;
-    case "OccupationalHealthcare":
-      details = <> 
-      
-      <div>Employer : {entry.employerName}</div>
-      {entry.sickLeave && 
-       <div>
-         start date: {entry.sickLeave.startDate} <br/>
-         end date: {entry.sickLeave.endDate}
-        </div>
-        }
-  
-      </>
-      break;
-    default:
-     return  assertNever(entry)
-   }
-  
-  return (
-    <>
-       <CommonDiagnosisEntry entry={entry}/>
-       {details}
-       <hr/>
-    </>
-  )
- }
- catch(err) {
-    let errorMessage="Error : "
-    if(err instanceof Error ) 
-      errorMessage+=err.message;
-    else 
-      errorMessage+=" Unknown Error : "+JSON.stringify(err,null,2);
-   console.log(`Error: ${errorMessage}`)
+      case "Hospital":
+        details = <>
+        {entry.discharge && (<>
+            <div>Date of Discharge :{entry.discharge.date}</div>
+            <div>Criteria for Discharge :{entry.discharge.criteria}</div>
+        </>)}
+        </>
+        break;
+      case "OccupationalHealthcare":
+        details = <> 
+        
+        <div>Employer : {entry.employerName}</div>
+        {entry.sickLeave && 
+        <div>
+          start date: {entry.sickLeave.startDate} <br/>
+          end date: {entry.sickLeave.endDate}
+          </div>
+          }
+    
+        </>
+        break;
+      default:
+      return  assertNever(entry)
+    }
+    
     return (
-      <li>
-        <h2>Entries Cannot Be Displayed Due to an Error !</h2>
-      </li>
-    );
+      <>
+        <CommonDiagnosisEntry entry={entry}/>
+        {details}
+        <hr/>
+      </>
+    )
   }
+  catch(err) {
+      let errorMessage="Error : "
+      if(err instanceof Error ) 
+        errorMessage+=err.message;
+      else 
+        errorMessage+=" Unknown Error : "+JSON.stringify(err,null,2);
+    console.log(`Error: ${errorMessage}`)
+      return (
+        <li>
+          <h2 style={{border:"red 0.4rem solid"}}>
+            Entries Cannot Be Displayed Right Now
+          </h2>
+        </li>
+      );
+    }
 
 }
 
