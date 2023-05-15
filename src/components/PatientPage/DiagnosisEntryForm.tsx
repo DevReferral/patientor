e7f248940e868eab42a0ec3b95c3579d04b36f65
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-15 05:28:19 +0000

    15 May 2023, 10:58

--- src/components/PatientPage/DiagnosisEntryForm.tsx
+++ src/components/PatientPage/DiagnosisEntryForm.tsx
@@ -1,3 +1,4 @@
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 import React, { useState } from 'react'
 import { DiagnosisEntry } from '../../types'
 
@@ -12,7 +13,22 @@ const DiagnosisEntryForm = () => {
      <h2>New {entryType} Entry</h2>
 
       {
-
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          // value={age}
+          label="Age"
+          // onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
 
       }
 
