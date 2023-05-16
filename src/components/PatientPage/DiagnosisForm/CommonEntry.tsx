30ae0153788f8c194e749ef2095302953e7520ad
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-16 04:25:34 +0000

    16 May 2023, 09:55

--- src/components/PatientPage/DiagnosisForm/CommonEntry.tsx
+++ src/components/PatientPage/DiagnosisForm/CommonEntry.tsx
@@ -9,15 +9,31 @@ const CommonEntry = ({formEntry,setFormEntry}:PropTypes) => {
    
  console.log("The form entry is ",JSON.stringify(formEntry,null,2));
 
+ const styles={
+  
+  inputForm:{ width:"50%", 
+  display: 'grid',
+   gridTemplateColumns: '0.5fr 1fr', 
+   gap: '1rem', 
+   alignItems:'flex-start' 
+  }
+
+ }
+
   return (
-   <>
-  <label htmlFor='description' >Description:</label>  
-  <input type='text' 
+ <div style={styles.inputForm}>
+  <label htmlFor='description'>Description:</label>
+  <input
+    type='text'
     id='description'
-    value={formEntry.description} 
-    onChange={({target})=>{setFormEntry(state => ({...state,description:target.value}))}}/>
-  <hr/>
-   </>
+    value={formEntry.description}
+    onChange={({ target }) => {
+      setFormEntry(state => ({ ...state, description: target.value }))
+    }}
+  />
+  <label htmlFor='date'>Date:</label>
+  <input type='date' id='date' defaultValue={new Date().toISOString().slice(0, 10)} />
+</div>
   )
 }
 
